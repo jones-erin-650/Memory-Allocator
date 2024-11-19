45b10e56fde2bfa6aae21006b29aa82f6d8f8b08
joneserin650 <joneserin650@gmail.com>
2024-11-19 00:02:22 -0500

    refactor: removed the allocatedProcesses and unallocatedProcesses lists from AlgorithmResponse because they weren't needed for this implementation

--- src/AlgorithmResponse.java
+++ src/AlgorithmResponse.java
@@ -1,17 +1,19 @@
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.LinkedList;
 import java.util.List;
 
 // This is necessary to keep the Processes read only. This way we can just add allocated processes to a response rather than edit the original Process
 
 public class AlgorithmResponse {
-    LinkedList<MemorySlot> outputMemory;
-    LinkedList<Process> outputProcesses;
-    List<Process> allocatedProcesses;
-    List<Process> unallocatedProcesses;
+    LinkedList<MemorySlot> outputMemory = new LinkedList<>();
+    LinkedList<Process> outputProcesses = new LinkedList<>();
 
     public AlgorithmResponse(LinkedList<MemorySlot> outputMemory, LinkedList<Process> outputProcesses) {
         this.outputMemory = outputMemory;
         this.outputProcesses = outputProcesses;
+    }
+
+    public AlgorithmResponse() {
     }
 
     public LinkedList<MemorySlot> getOutputMemory() {
